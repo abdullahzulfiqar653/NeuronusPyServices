f5df054b713014592ae27e170bf3c2b5753ea300
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-05-13 12:18:42 +0500

    updates in email extension

--- NeuroMail/admin/mailbox.py
+++ NeuroMail/admin/mailbox.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from NeuroMail.models.mailbox import MailBox
+from NeuroMail.models.email_extension import EmailExtension
 
 
 class MailBoxAdmin(admin.ModelAdmin):
@@ -8,3 +9,10 @@ class MailBoxAdmin(admin.ModelAdmin):
 
 
 admin.site.register(MailBox, MailBoxAdmin)
+
+
+class EmailExtensionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+admin.site.register(EmailExtension, EmailExtensionAdmin)
--- NeuroMail/models/email_extension.py
+++ NeuroMail/models/email_extension.py
@@ -5,6 +5,7 @@ from main.models.abstract.base import BaseModel
 
 class EmailExtension(BaseModel):
     name = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name}"
