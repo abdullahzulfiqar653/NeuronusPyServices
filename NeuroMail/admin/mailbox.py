134228f29884b5fceeed03bdc455acdbc4ab6440
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2025-07-17 12:26:14 +0500

    admin added for creating recipients

--- NeuroMail/admin/mailbox.py
+++ NeuroMail/admin/mailbox.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from NeuroMail.models.mailbox import MailBox
+from NeuroMail.models.email_recipient import EmailRecipient
 from NeuroMail.models.email_extension import EmailExtension
 
 
@@ -9,6 +10,15 @@ class MailBoxAdmin(admin.ModelAdmin):
 
 
 admin.site.register(MailBox, MailBoxAdmin)
+
+
+class EmailRecipientAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "name", "recipient_type")
+    search_fields = ("email",)
+    autocomplete_fields = ("mail",)
+
+
+admin.site.register(EmailRecipient, EmailRecipientAdmin)
 
 
 class EmailExtensionAdmin(admin.ModelAdmin):
