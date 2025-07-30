979af9ec09aaba8458b1235f727e3133dc677c5f
Hiba-Shahid <hibashahid678@gmail.com>
2025-07-30 17:31:04 +0500

    updated views

--- NeuroMail/views/email_tracking.py
+++ NeuroMail/views/email_tracking.py
@@ -1,33 +1,33 @@
-from rest_framework import generics
-from django.http import HttpResponse
-from django.utils import timezone
-from NeuroMail.models.email import Email
 import base64
+from django.utils import timezone
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from NeuroMail.models.email import Email
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
-class EmailTrackingPixelView(generics.RetrieveAPIView):
-    """
-    Endpoint to track if recipient has viewed the email.
-    Returns a 1x1 transparent PNG pixel.
-    """
+class EmailTrackingPixelView(APIView):
 
-    queryset = Email.objects.all()
-    lookup_field = "id"
-    permission_classes = [] 
+    TRANSPARENT_PIXEL = base64.b64decode(
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
+    )
 
-    def retrieve(self, request, *args, **kwargs):
-        email = self.get_object()
+    @swagger_auto_schema(
+        operation_description="Tracking pixel to mark email as viewed.",
+        manual_parameters=[
+            openapi.Parameter('email_id', openapi.IN_PATH, description="Email ID", type=openapi.TYPE_STRING)
+        ],
+        responses={200: '1x1 Transparent PNG'}
+    )
+    def get(self, request, email_id):
+        try:
+            email = Email.objects.get(id=email_id)
+            if not email.is_viewed_by_recepient:
+                email.is_viewed_by_recepient = True
+                email.viewed_at = timezone.now()
+                email.save()
+        except Email.DoesNotExist:
+            pass
 
-        if not email.is_viewed_by_recepient:
-            email.is_viewed_by_recepient = True
-            email.viewed_at = timezone.now()  
-            email.save(update_fields=["is_viewed_by_recepient", "viewed_at"])
-
-        print(f"[PIXEL TRACKED] Email ID: {email.id}, IP: {request.META.get('REMOTE_ADDR')}")
-
-        # Base64 for a 1x1 transparent PNG
-        transparent_pixel = base64.b64decode(
-            "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
-        )
-        return HttpResponse(transparent_pixel, content_type="image/png")
-
+        return HttpResponse(self.TRANSPARENT_PIXEL, content_type="image/png")
