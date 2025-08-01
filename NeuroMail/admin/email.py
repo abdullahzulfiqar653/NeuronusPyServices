99add9d1ff3bfa07c14a6d0facd50c0d420fd702
Hiba-Shahid <hibashahid678@gmail.com>
2025-08-01 08:30:57 +0500

    changed uid

--- NeuroMail/admin/email.py
+++ NeuroMail/admin/email.py
@@ -22,7 +22,7 @@ class EmailRecipientInline(admin.TabularInline):
 class EmailAdmin(admin.ModelAdmin):
     list_display = (
         "id",
-        "uid",
+        "imap_id",
         "subject",
         "email_type",
         "primary_email_type",
