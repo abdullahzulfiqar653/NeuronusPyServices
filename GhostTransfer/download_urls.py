740e9229424aa78ecbd1d0bb6aac6515f746c327
Hamid Javed <hamidjaved615@gmail.com>
2026-01-14 22:12:30 +0500

    fix the code

--- GhostTransfer/download_urls.py
+++ GhostTransfer/download_urls.py
@@ -3,5 +3,4 @@ from GhostTransfer.views import FileShareAccessView
 
 urlpatterns = [
     path("<str:pk>/", FileShareAccessView.as_view(), name="fileshare-access"),
-    # path("<str:pk>", FileShareAccessView.as_view(), name="fileshare-access-no-slash"),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from django.contrib import admin
-from GhostTransfer.views import FileShareAccessView
 
-urlpatterns = [ 
+
+urlpatterns = [
     path("admin/", admin.site.urls),
-    path("<str:pk>", FileShareAccessView.as_view(), name="fileshare-access-no-slash"),
-
 ]
