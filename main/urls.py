9d7356486b4814bf5e529ac9cb70365e0d62fed0
Hamid Javed <hamidjaved615@gmail.com>
2026-01-15 16:07:01 +0500

    text decryption

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from django.contrib import admin
-from GhostTransfer.views import FileShareAccessView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("<str:pk>/", FileShareAccessView.as_view(), name="fileshare-access"),
-    
 ]
